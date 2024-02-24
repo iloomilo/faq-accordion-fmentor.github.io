ac29cd1abe3f18b574d40aabb72bf5d730f6092b
iloomilo <154640529+iloomilo@users.noreply.github.com>
2024-02-24 03:08:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".hidden").forEach((e) => {
+    console.log(e);
+    e.querySelectorAll("img").forEach((element) => {
+        element.addEventListener("click", () => {
+            e.classList.toggle("hidden");
+        })
+    })
+    
+});
